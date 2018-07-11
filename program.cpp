14aab947e339a94010abbdfcbae8f3f996c15bd6
Michał Radecki <mr371591@students.mimuw.edu.pl>
2018-07-11 20:36:47 +0200

    Dodawanie 3 składników

--- program.cpp
+++ program.cpp
@@ -9,10 +9,14 @@ public:
 	double dodaj(double x, double y) {
 		return x + y;
 	}
+	double dodaj(double x, double y, double z) {
+		return dodaj(x, y) + z;
+	}
 };
 
 void test_dodawania(Kalkulator calc) {
-	cout << calc.dodaj(1, 2);
+	cout << calc.dodaj(1, 2) << endl;
+	cout << calc.dodaj(1, 2, 3) << endl;
 }
 
 int main() {
