dfcb8f106b97444b8a7c718f8219b923ee625ac1
Michał Radecki <mr371591@students.mimuw.edu.pl>
2018-07-11 20:42:24 +0200

    Mnozenie 3 składników

--- program.cpp
+++ program.cpp
@@ -9,10 +9,18 @@ public:
 	double dodaj(double x, double y) {
 		return x + y;
 	}
+	double mnoz(double x, double y) {
+		return x * y;
+	}
+	double mnoz(double x, double y, double z) {
+		return mnoz(x, y) * z;
+	}
 };
 
 void test_dodawania(Kalkulator calc) {
-	cout << calc.dodaj(1, 2);
+	cout << calc.dodaj(1, 2) << endl;
+	cout << calc.mnoz(3, 4) << endl;
+	cout << calc.mnoz(3, 4, 5) << endl;
 }
 
 int main() {
